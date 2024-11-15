9cd02ae91231033cf06edf41b7eb49bd0ef7f3f2
Duarte Chen <duartechen@gmail.com>
2024-11-15 10:58:16 +0000

    Erro ao criar o webservice

--- src/main/java/AddServerIntf.java
+++ src/main/java/AddServerIntf.java
@@ -2,4 +2,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 public interface AddServerIntf extends Remote {
   String listarConsultas(String idClient) throws RemoteException;
+  String marcarConsulta(int dia, int mes, int ano, int hora, int clientID, int clinicaID, int especialidadeID) throws RemoteException;
+  
+  //String listarClinicas() throws RemoteException;
+  //String listarEspecialidades(int idClinica) throws RemoteException;
 }
--- src/main/java/FrontEnd_Clinica.java
+++ src/main/java/FrontEnd_Clinica.java
@@ -23,4 +23,46 @@ public class FrontEnd_Clinica {
 		return addServerIntf.listarConsultas(idClicentString);
 
     }
+    
+    public String marcarConsultas_Server(int dia, int mes, int ano, int hora, int clientID, int clinicaID, int especialidadeID) throws MalformedURLException, RemoteException, NotBoundException {
+    	   
+    	
+    	String addServerURL = "rmi://" + "192.168.128.2" + "/AddServer";
+    	AddServerIntf addServerIntf = (AddServerIntf)Naming.lookup(addServerURL);
+    	
+  
+        
+
+		return addServerIntf.marcarConsulta(dia, mes, ano, hora, clientID, clinicaID, especialidadeID);
+
+    }
+    
+    /*
+    public String listarClinicas_Server() throws MalformedURLException, RemoteException, NotBoundException {
+    	   
+    	
+    	String addServerURL = "rmi://" + "192.168.128.2" + "/AddServer";
+    	AddServerIntf addServerIntf = (AddServerIntf)Naming.lookup(addServerURL);
+    	
+  
+        
+
+		return addServerIntf.listarClinicas();
+
+    }
+    
+    public String listarEspecialidades_Server(int idClinica) throws MalformedURLException, RemoteException, NotBoundException {
+    	   
+    	
+    	String addServerURL = "rmi://" + "192.168.128.2" + "/AddServer";
+    	AddServerIntf addServerIntf = (AddServerIntf)Naming.lookup(addServerURL);
+    	
+  
+        
+
+		return addServerIntf.listarEspecialidades(idClinica);
+
+    }
+*/    
+    
 }
